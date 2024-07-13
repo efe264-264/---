c0965b7296ccfe84bd0cbd10d87a1864953a910c
aaaaxxxxaaaa <153498331+aaaaxxxxaaaa@users.noreply.github.com>
2024-07-13 22:49:50 +0800

    Add files via upload

--- manager.cpp
+++ manager.cpp
@@ -1,223 +1,239 @@
+#include<QSql>
+#include<QSqlQuery>
+#include<QsqlError>
+#include<QDebug>
+#include<QString>
 #include"manager.h"
 
 course_manager::course_manager()
 {
-	con = mysql_init(NULL);
-	//�����ַ�����
-	mysql_options(con, MYSQL_SET_CHARSET_NAME, "GBK");
+    DB=QSqlDatabase::addDatabase("QMYSQL");//加载mysql驱动
+    DB.setDatabaseName("course_manager");                      //数据库名字
+    DB.setHostName("localhost");                   //数据库地址，本地默认localhost
+    DB.setUserName("root");                            //用户名，本地默认root
+    DB.setPassword("xu2365651");                       //密码，你自己设置的那个
+    if(!DB.open())
+    {
+        qDebug()<<"Error: Connection with database failed: "<<DB.lastError().text();
 
-	if (!mysql_real_connect(con, host, user, pw, database_name, port, NULL, 0))
-	{
-		exit(1);
-	}
+    }
 }
 
 course_manager::~course_manager()
 {
-	mysql_close(con);
+    DB.close();
 }
 
 
-bool course_manager::insert_course(course& cs)
+/*bool course_manager::insert_course(course& cs)
 {
-	char sql[1024];
-	sprintf_s(sql, "insert into courses(course_id,course_name,course_credit,class_num,after_course,course_class1,course_class2,course_class3,total1,total2,total3,allowance1,allowance2,allowance3,choice) values(%d,'%s',%d,%d,'%s','%s','%s','%s',%d,%d,%d,%d,%d,%d,'%s')",
-		cs.course_id, cs.course_name.c_str(), cs.course_credit, cs.class_num, cs.after_course.c_str(),cs.course_class[0].c_str(),cs.course_class[1].c_str(),cs.course_class[2].c_str(),cs.total[0], cs.total[1], cs.total[2],cs.allowance[0], cs.allowance[1], cs.allowance[2], cs.choice.c_str());
-	
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to insert data : Error:%s\n", mysql_error(con));
-		return false;
-	}
-	return true;
-}
 
-bool course_manager::insert_student(student& stu)
+
+    char sql[1024];
+    sprintf_s(sql, "insert into courses(course_id,course_name,course_credit,class_num,after_course,course_class1,course_class2,course_class3,total1,total2,total3,allowance1,allowance2,allowance3,choice) values(%d,'%s',%d,%d,'%s','%s','%s','%s',%d,%d,%d,%d,%d,%d,'%s')",
+              cs.course_id, cs.course_name.c_str(), cs.course_credit, cs.class_num, cs.after_course.c_str(),cs.course_class[0].c_str(),cs.course_class[1].c_str(),cs.course_class[2].c_str(),cs.total[0], cs.total[1], cs.total[2],cs.allowance[0], cs.allowance[1], cs.allowance[2], cs.choice.c_str());
+
+    if (mysql_query(con, sql))
+    {
+        fprintf(stderr, "Failed to insert data : Error:%s\n", mysql_error(con));
+        return false;
+    }
+    return true;
+}*/
+
+/*bool course_manager::insert_student(student& stu)
 {
-	char sql[1024];
-	sprintf_s(sql, "insert into students(student_id,student_name,course_class1,course_class2,course_class3,course_class4,course_class5,course_class6,course_class7,course_class8) values(%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s')",
-		stu.student_id, stu.student_name.c_str(), stu.course_class[0].c_str(), stu.course_class[1].c_str(), stu.course_class[2].c_str(), stu.course_class[3].c_str(), stu.course_class[4].c_str(), stu.course_class[5].c_str(), stu.course_class[6].c_str(), stu.course_class[7].c_str());
+    char sql[1024];
+    sprintf_s(sql, "insert into students(student_id,student_name,course_class1,course_class2,course_class3,course_class4,course_class5,course_class6,course_class7,course_class8) values(%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s')",
+              stu.student_id, stu.student_name.c_str(), stu.course_class[0].c_str(), stu.course_class[1].c_str(), stu.course_class[2].c_str(), stu.course_class[3].c_str(), stu.course_class[4].c_str(), stu.course_class[5].c_str(), stu.course_class[6].c_str(), stu.course_class[7].c_str());
 
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to insert data : Error:%s\n", mysql_error(con));
-		return false;
-	}
-	return true;
-}
+    if (mysql_query(con, sql))
+    {
+        fprintf(stderr, "Failed to insert data : Error:%s\n", mysql_error(con));
+        return false;
+    }
+    return true;
+}*/
 
 bool course_manager::update_course(course& cs)
 {
-	char sql[1024];
-	sprintf_s(sql, "update courses set course_name = '%s',course_credit = '%d',class_num='%d',after_course='%s',course_class1='%s',course_class2='%s',course_class3='%s',total1='%d',total2='%d',total3='%d',allowance1='%d',allowance2='%d',allowance3='%d',choice='%s'"
-		"where course_id = %d", cs.course_name.c_str(), cs.course_credit, cs.class_num, cs.after_course.c_str(), cs.course_class[0].c_str(), cs.course_class[1].c_str(), cs.course_class[2].c_str(), cs.total[0], cs.total[1], cs.total[2], cs.allowance[0], cs.allowance[1], cs.allowance[2], cs.choice.c_str(), cs.course_id);
-
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to update data : Error:%s\n", mysql_error(con));
-		return false;
-	}
-	return true;
+    QSqlQuery query;
+    bool ok;
+    ok = query.exec(QString("UPDATE courses SET allowance1='%1',allowance2='%2',allowance3='%3',choice='%4' WHERE course_id=%5")
+                        .arg(cs.allowance[0]).arg(cs.allowance[1]).arg(cs.allowance[2]).arg(cs.choice).arg(cs.course_id));
+    if (!ok)
+        qDebug() << "Update error:" << query.lastError().text();
+    return ok;
 }
 
 bool course_manager::update_student(student& stu,int i)
 {
-	char sql[1024];
-	if (i == 1)
-	{
-		sprintf_s(sql, "update students set course_class1 = '%s'"
-			"where student_id = %d", stu.course_class[0].c_str(), stu.student_id);
-	}
-	else if (i == 2)
-	{
-		sprintf_s(sql, "update students set course_class2 = '%s'"
-			"where student_id = %d", stu.course_class[1].c_str(), stu.student_id);
-	}
-	else if (i == 3)
-	{
-		sprintf_s(sql, "update students set course_class3 = '%s'"
-			"where student_id = %d", stu.course_class[2].c_str(), stu.student_id);
-	}
-	else if (i == 4)
-	{
-		sprintf_s(sql, "update students set course_class4 = '%s'"
-			"where student_id = %d", stu.course_class[3].c_str(), stu.student_id);
-	}
-	else if (i == 5)
-	{
-		sprintf_s(sql, "update students set course_class5 = '%s'"
-			"where student_id = %d", stu.course_class[4].c_str(), stu.student_id);
-	}
-	else if (i == 6)
-	{
-		sprintf_s(sql, "update students set course_class6 = '%s'"
-			"where student_id = %d", stu.course_class[5].c_str(), stu.student_id);
-	}
-	else if (i == 7)
-	{
-		sprintf_s(sql, "update students set course_class7 = '%s'"
-			"where student_id = %d", stu.course_class[6].c_str(), stu.student_id);
-	}
-	else
-	{
-		sprintf_s(sql, "update students set course_class8 = '%s'"
-			"where student_id = %d", stu.course_class[7].c_str(), stu.student_id);
-	}
+    QSqlQuery query;
+    bool ok;
 
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to update data : Error:%s\n", mysql_error(con));
-		return false;
-	}
-	return true;
+    if (i == 1)
+    {
+        ok = query.exec(QString("UPDATE students SET course_class1='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[0]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class1 = '%s'"
+        //               "where student_id = %d", stu.course_class[0].c_str(), stu.student_id);
+    }
+    else if (i == 2)
+    {
+        ok = query.exec(QString("UPDATE students SET course_class2='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[1]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class2 = '%s'"
+          //             "where student_id = %d", stu.course_class[1].c_str(), stu.student_id);
+    }
+    else if (i == 3)
+    {
+        ok = query.exec(QString("UPDATE students SET course_class3='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[2]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class3 = '%s'"
+          //             "where student_id = %d", stu.course_class[2].c_str(), stu.student_id);
+    }
+    else if (i == 4)
+    {
+        ok = query.exec(QString("UPDATE students SET course_class4='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[3]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class4 = '%s'"
+          //             "where student_id = %d", stu.course_class[3].c_str(), stu.student_id);
+    }
+    else if (i == 5)
+    {
+        ok = query.exec(QString("UPDATE students SET course_class5='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[4]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class5 = '%s'"
+          //             "where student_id = %d", stu.course_class[4].c_str(), stu.student_id);
+    }
+    else if (i == 6)
+    {
+        ok = query.exec(QString("UPDATE students SET course_class6='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[5]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class6 = '%s'"
+          //             "where student_id = %d", stu.course_class[5].c_str(), stu.student_id);
+    }
+    else if (i == 7)
+    {
+        ok = query.exec(QString("UPDATE students SET course_class7='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[6]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class7 = '%s'"
+          //             "where student_id = %d", stu.course_class[6].c_str(), stu.student_id);
+    }
+    else
+    {
+        ok = query.exec(QString("UPDATE students SET course_class8='%1' WHERE student_id=%2")
+                            .arg(stu.course_class[7]).arg(stu.student_id));
+        //sprintf_s(sql, "update students set course_class8 = '%s'"
+          //             "where student_id = %d", stu.course_class[7].c_str(), stu.student_id);
+    }
+
+    if (!ok)
+        qDebug() << "Update error:" << query.lastError().text();
+    return ok;
 }
 
-bool course_manager::delete_course(int course_id)
+bool course_manager::get_student(student& stu, QString condition)
 {
-	char sql[1024];
-	sprintf_s(sql, "delete from courses where course_id = %d",course_id);
+    QSqlQuery query;
+    if (query.exec("SELECT * FROM students"+condition))
+    {
+        query.next();
+        stu.student_id = query.value(0).toInt(); // 假设第一列是name
+        stu.student_name  = query.value(1).toString(); // 假设第二列是age
+        stu.course_class[0] = query.value(2).toString();
+        stu.course_class[1] = query.value(3).toString();
+        stu.course_class[2] = query.value(4).toString();
+        stu.course_class[3] = query.value(5).toString();
+        stu.course_class[4] = query.value(6).toString();
+        stu.course_class[5] = query.value(7).toString();
+        stu.course_class[6] = query.value(8).toString();
+        stu.course_class[7] = query.value(9).toString();
 
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to delete data : Error:%s\n", mysql_error(con));
-		return false;
-	}
-	return true;
+    } else {
+        qDebug() << "Select error:" << query.lastError().text();
+        return false;
+    }
+
+    return true;
+
 }
 
-vector<course> course_manager::get_course(string condition)
+/*bool course_manager::delete_course(int course_id)
 {
-	vector<course> cslist;
-	char sql[1024];
-	sprintf_s(sql, "select * from courses %s", condition.c_str());
+    char sql[1024];
+    sprintf_s(sql, "delete from courses where course_id = %d",course_id);
 
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to selete data : Error:%s\n", mysql_error(con));
-		return {};
-	}
-	MYSQL_RES* res = mysql_store_result(con);
-	MYSQL_ROW row;
-	while (row = mysql_fetch_row(res))
-	{
-		course cs;
-		cs.course_id = atoi(row[0]);
-		cs.course_name = row[1];
-		cs.course_credit = atoi(row[2]);
-		cs.class_num= atoi(row[3]);
-		cs.after_course = row[4];
-		cs.course_class[0] = row[5];
-		cs.course_class[1] = row[6];
-		cs.course_class[2] = row[7];
-		cs.total[0] = atoi(row[8]);
-		cs.total[1] = atoi(row[9]);
-		cs.total[2] = atoi(row[10]);
-		cs.allowance[0] = atoi(row[11]);
-		cs.allowance[1] = atoi(row[12]);
-		cs.allowance[2] = atoi(row[13]);
-		cs.choice = row[14];
+    if (mysql_query(con, sql))
+    {
+        fprintf(stderr, "Failed to delete data : Error:%s\n", mysql_error(con));
+        return false;
+    }
+    return true;
+}*/
 
-		cslist.push_back(cs);
-	}
-	return cslist;
-}
-
-bool course_manager::get_a_course(course& cs, string condition)
+vector<course> course_manager::get_course(QString condition)
 {
-	char sql[1024];
-	sprintf_s(sql, "select * from courses %s", condition.c_str());
+    vector<course>cslist;
+    QSqlQuery query;
+    if (query.exec("SELECT * FROM courses"+condition))
+    {
+        while(query.next())
+        {
+            course cs;
+            cs.course_id = query.value(0).toInt(); // 假设第一列是name
+            cs.course_name  = query.value(1).toString(); // 假设第二列是age
+            cs.course_credit = query.value(2).toInt();
+            cs.class_num = query.value(3).toInt();
+            cs.after_course = query.value(4).toString();
+            cs.course_class[0] = query.value(5).toString();
+            cs.course_class[1] = query.value(6).toString();
+            cs.course_class[2] = query.value(7).toString();
+            cs.total[0] = query.value(8).toInt();
+            cs.total[1] = query.value(9).toInt();
+            cs.total[2] = query.value(10).toInt();
+            cs.allowance[0] = query.value(11).toInt();
+            cs.allowance[1] = query.value(12).toInt();
+            cs.allowance[2] = query.value(13).toInt();
+            cs.choice = query.value(14).toString();
+            cslist.push_back(cs);
+        }
 
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to selete data : Error:%s\n", mysql_error(con));
-		return false;
-	}
-	MYSQL_RES* res = mysql_store_result(con);
-	MYSQL_ROW row;
-	row = mysql_fetch_row(res);
-	cs.course_id = atoi(row[0]);
-	cs.course_name = row[1];
-	cs.course_credit = atoi(row[2]);
-	cs.class_num = atoi(row[3]);
-	cs.after_course = row[4];
-	cs.course_class[0] = row[5];
-	cs.course_class[1] = row[6];
-	cs.course_class[2] = row[7];
-	cs.total[0] = atoi(row[8]);
-	cs.total[1] = atoi(row[9]);
-	cs.total[2] = atoi(row[10]);
-	cs.allowance[0] = atoi(row[11]);
-	cs.allowance[1] = atoi(row[12]);
-	cs.allowance[2] = atoi(row[13]);
-	cs.choice = row[14];
-	return true;
+    } else {
+        qDebug() << "Select error:" << query.lastError().text();
+        return {};
+    }
+
+    return cslist;
 }
 
-bool course_manager::get_student(student& stu, string condition)
+bool course_manager::get_a_course(course& cs, QString condition)
 {
-	char sql[1024];
-	sprintf_s(sql, "select * from students %s", condition.c_str());
+    QSqlQuery query;
+    if (query.exec("SELECT * FROM courses"+condition))
+    {
+            query.next();
+            cs.course_id = query.value(0).toInt();
+            cs.course_name  = query.value(1).toString();
+            cs.course_credit = query.value(2).toInt();
+            cs.class_num = query.value(3).toInt();
+            cs.after_course = query.value(4).toString();
+            cs.course_class[0] = query.value(5).toString();
+            cs.course_class[1] = query.value(6).toString();
+            cs.course_class[2] = query.value(7).toString();
+            cs.total[0] = query.value(8).toInt();
+            cs.total[1] = query.value(9).toInt();
+            cs.total[2] = query.value(10).toInt();
+            cs.allowance[0] = query.value(11).toInt();
+            cs.allowance[1] = query.value(12).toInt();
+            cs.allowance[2] = query.value(13).toInt();
+            cs.choice = query.value(14).toString();
 
-	if (mysql_query(con, sql))
-	{
-		fprintf(stderr, "Failed to selete data : Error:%s\n", mysql_error(con));
-		return false;
-	}
-	MYSQL_RES* res = mysql_store_result(con);
-	MYSQL_ROW row;
-	row = mysql_fetch_row(res);
-	stu.student_id = atoi(row[0]);
-	stu.student_name = row[1];
-	stu.course_class[0] = row[2];
-	stu.course_class[1] = row[3];
-	stu.course_class[2] = row[4];
-	stu.course_class[3] = row[5];
-	stu.course_class[4] = row[6];
-	stu.course_class[5] = row[7];
-	stu.course_class[6] = row[8];
-	stu.course_class[7] = row[9];
-	return true;
+
+    } else {
+        qDebug() << "Select error:" << query.lastError().text();
+        return false;
+    }
+
+    return true;
 }
-
-
-
--- manager.h
+++ manager.h
@@ -1,33 +1,47 @@
-#pragma once
-#include<mysql.h>
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include<QSqlDatabase>
 #include<iostream>
 #include<string>
 #include<vector>
 #include<stdio.h>
 #include<vector>
-#include"course.h"
+#include<QString>
 #include"student.h"
 using namespace std;
+
+struct course
+{
+    int course_id;
+    QString course_name;
+    int course_credit;
+    int class_num;
+    QString after_course;
+    QString course_class[3];//课头记录老师名字
+    int total[3];//记录每个课头学生总量
+    int allowance[3];//记录每个课头的余量
+    QString choice;
+    //course(int id, QString name, int cre, int cl, QString ac,QString c);
+    //course();
+};
 
 
 class course_manager
 {
 public:
-	course_manager();
-	~course_manager();
-	bool insert_course(course& cs);
-	bool insert_student(student& stu);
-	bool update_course(course& cs);
-	bool update_student(student& stu,int i);
-	bool delete_course(int course_id);
-	vector<course> get_course(string condition = "");
-	bool get_a_course(course& cs, string condition = "");
-	bool get_student(student& stu, string condition = "");
+    course_manager();
+    ~course_manager();
+    bool insert_course(course& cs);
+    bool insert_student(student& stu);
+    bool update_course(course& cs);
+    bool update_student(student& stu,int i);
+    bool delete_course(int course_id);
+    vector<course> get_course(QString condition = "");
+    bool get_a_course(course& cs, QString condition = "");
+    bool get_student(student& stu, QString condition = "");
 private:
-	MYSQL* con;
-	const char* host = "127.0.0.1";
-	const char* user = "root";
-	const char* pw = "xu2365651";
-	const char* database_name = "course_manager";
-	const int port = 3306;
+    QSqlDatabase DB;
 };
+
+#endif // MANAGER_H
